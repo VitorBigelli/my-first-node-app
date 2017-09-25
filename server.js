e8470be605a5e21811a489298bcb17ddcc63dd8f
Vitor <vitorbigelli@hotmail.com>
2017-09-25 08:19:06 -0300

    Create server file

--- server.js
+++ server.js
@@ -9,3 +9,21 @@ var app = express();
 app.listen(3000, function() {
 	console.log("Server is listening on port 3000");
 })
+
+// 
+app.get(`/`, function(request, response) {
+	response.send('Return the index page');
+});
+
+app.get("/messages", function(request, response) {
+	response.send('Return a list of all stored messages');
+});
+
+app.post("/messages", function(request, response) {
+	response.send('Store a new message');
+});
+
+app.delete("/message", function(request, response) {
+	response.send('Delete a specific message');
+});
+
